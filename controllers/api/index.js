8caa3da2ff4c28c52d986e51174cc97534a22835
FemBot 9000 <paige.el.olsen@gmail.com>
2021-10-27 10:48:14 -0700

    adding all folders and files

---
+++ controllers/api/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const  = require("./");
+router.use("/",);
+
+const  = require("./");
+router.use("/",);
+
+module.exports = router;
